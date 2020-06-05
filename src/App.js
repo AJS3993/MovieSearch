16be8d88143494a98b1613b734e18d4983a385a9
Andrew Smith <ajs3993@gmail.com>
2020-06-05 09:10:11 -0500

    search input saved

--- src/App.js
+++ src/App.js
@@ -1,15 +1,38 @@
-import React from 'react';
-import Search from './components/Search'
+import React, {useState} from 'react';
+import Search from './components/Search';
+import axios from 'axios';
 
 
 function App() {
+  const [state, setState] = useState({
+    s:"",
+    results:[],
+    selected: {}
+  });
+
+  const apiurl = 'http://www.omdbapi.com/?i=tt3896198&apikey=e13ab5d';
+
+  const search = (e) => {
+    if (e.key === "Enter"){
+
+    }
+  }
+
+  const handleInput = (e) => {
+    let s = e.target.value;
+
+    setState(prevState => {
+      return {...prevState, s:s}
+    })
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>Movie Search</h1>
       </header>
       <main>
-        <Search />
+        <Search handleInput={handleInput}/>
       </main>
     </div>
   );
